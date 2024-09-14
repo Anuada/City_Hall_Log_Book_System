c2c4da895319070dfaf5aebccb81893cd5dcef6d
Dominic Navos <dominicnavos@gmail.com>
2024-09-15 01:00:41 +0800

    updated db

--- util/city_log_booksystem.sql
+++ util/city_log_booksystem.sql
@@ -21,16 +21,28 @@ CREATE TABLE IF NOT EXISTS `visitor_info` (
   `lname` varchar(100) NOT NULL,
   `purpose` varchar(100) NOT NULL,
   `type` enum('Employee','Visitor') CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL DEFAULT 'Visitor',
+  `status` enum('Pending','Accepted','Cancelled') CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL DEFAULT 'Pending',
   `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=35 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=59 DEFAULT CHARSET=latin1;
 
--- Dumping data for table city_log_booksystem.visitor_info: ~4 rows (approximately)
-INSERT INTO `visitor_info` (`id`, `fname`, `lname`, `purpose`, `type`, `date`) VALUES
-	(31, 'Ericson', 'Anuada', 'Internship', 'Employee', '2024-09-11 09:32:20'),
-	(32, 'French', 'Butera', 'Apply', 'Visitor', '2024-09-11 09:39:42'),
-	(33, 'Torya', 'Bulacao', 'I work here', 'Employee', '2024-09-11 09:55:59'),
-	(34, 'Mardon', 'Dela Peña', 'Paper Works', 'Visitor', '2024-09-11 10:23:48');
+-- Dumping data for table city_log_booksystem.visitor_info: ~15 rows (approximately)
+INSERT INTO `visitor_info` (`id`, `fname`, `lname`, `purpose`, `type`, `status`, `date`) VALUES
+	(31, 'Ericson', 'Anuada', 'Internship', 'Employee', 'Accepted', '2024-09-11 09:32:20'),
+	(32, 'French', 'Butera', 'Apply', 'Visitor', 'Accepted', '2024-09-11 09:39:42'),
+	(33, 'Torya', 'Bulacao', 'I work here', 'Employee', 'Accepted', '2024-09-11 09:55:59'),
+	(34, 'Mardon', 'Dela Peña', 'Paper Works', 'Visitor', 'Pending', '2024-09-11 10:23:48'),
+	(35, 'Ariana', 'Grande', 'Working', 'Employee', 'Pending', '2024-09-11 11:48:29'),
+	(36, 'Karl John', 'Delfin', 'License', 'Visitor', 'Pending', '2024-09-11 12:11:55'),
+	(37, 'Dweight Dewey', 'Fuentes', 'License', 'Visitor', 'Pending', '2024-09-11 13:22:22'),
+	(38, 'Ariana', 'Grande', 'Snack', 'Visitor', 'Accepted', '2024-09-11 17:38:58'),
+	(39, 'Zhuang', 'Cruz', 'Deed of Sale', 'Visitor', 'Accepted', '2024-09-12 02:44:21'),
+	(40, 'French', 'Butera', 'Application', 'Employee', 'Accepted', '2024-09-12 12:19:53'),
+	(41, 'Ariana', 'Grande', 'Work', 'Employee', 'Accepted', '2024-09-13 12:54:43'),
+	(42, 'Mardon', 'Dela Peña', 'Application', 'Employee', 'Accepted', '2024-09-13 12:57:30'),
+	(48, 'Ericson', 'Anuada', 'Work', 'Employee', 'Accepted', '2024-09-14 06:09:40'),
+	(56, 'Zhuang', 'Cruz', 'Malibang', 'Employee', 'Accepted', '2024-09-14 10:58:50'),
+	(57, 'Ariana', 'Grande', 'Woah', 'Visitor', 'Accepted', '2024-09-14 11:22:51');
 
 /*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
