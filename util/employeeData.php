60a1dee8846ae741d0b5534a44c38b77e185cd10
Dominic Navos <dominicnavos@gmail.com>
2024-10-04 16:15:42 +0800

    fixed error?

--- util/employeeData.php
+++ util/employeeData.php
@@ -2,26 +2,23 @@
 
 $url = 'https://www.cebucity.gov.ph/pitchV2/employees.php/employees';
 
-$headers = @get_headers($url);
-
-if ($headers === false || strpos($headers[0], '200') === false) {
-    http_response_code(500);
-    echo json_encode(['message' => "We're having trouble connecting to the server. Please try again later."]);
-    exit();
-}
+header("Content-Type: application/json");
 
 $context = stream_context_create([
     'ssl' => [
         'verify_peer' => false,
         'verify_peer_name' => false,
-    ]
+    ],
 ]);
 
-$response = file_get_contents($url, false, $context);
-
-if ($response === false) {
+try {
+    $response = @file_get_contents($url, false, $context);
+    if ($response === false) {
+        throw new Exception('Request Error: Unable to fetch data');
+    }
+} catch (Exception $e) {
     http_response_code(500);
-    echo json_encode(['message' => 'Request Error: Unable to fetch data']);
+    echo json_encode(['message' => "We're having trouble connecting to the server. Please try again later."]);
     exit();
 }
 
