4800f1847734ab267b087b967a93ebaaa425e643
Dominic Navos <dominicnavos@gmail.com>
2024-10-05 12:20:05 +0800

    added animation to fetch employee data loader

--- assets/js/misc/loaders.js
+++ assets/js/misc/loaders.js
@@ -5,4 +5,8 @@ export const loader = `
         <span></span>
     </div>
 `;
-export const loader2 = `<i class="text-muted">fetching employee data...</i>`;
+export const loader2 = `
+    <i class="text-muted">
+        fetching employee data<span class="dot1">.</span><span class="dot2">.</span><span class="dot3">.</span>
+    </i>
+`;
